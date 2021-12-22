91e4ce211cfada0d9e0959220536c8740f98f45a
Gabriel Cruz <cruzg3dev@gmail.com>
2021-12-22 14:01:25 -0300

    aula1: js

---
+++ tracoParkingApp/controller/cadastro.js
@@ -0,0 +1,13 @@
+
+const formulario = document.getElementById('formulario');
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const cadastroCliente = {
+        owner: document.getElementById('name'),
+        model: document.getElementById('modelo'),
+        type: document.getElementById('tipo'),
+        label: document.getElementById('placa'),
+        observation:   document.getElementById('observacoes')
+    }
+})
